c24b516a49e2dc828d9d296e9183a4b772279297
Katheryn <Katheryngbarreto@gmil.com>
2022-12-07 15:26:41 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,72 @@
-alert("hola")
+alert("hola!");
+/*
+
+Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page
+ that rotates unique colors and content for each weekday (Sunday to Saturday) 
+ into the page.  The content must include:
+
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph 
+    must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+    
+
+
+    The items we need are:
+    image (a pic of the coffee)
+    alt ( a description of the pic for people who cant see it )
+
+
+     The unique color could affect the background HTML, or an element on the page 
+     for each day of the week.  All of the above must occur within one page.
+*/
+
+function coffeeTemplate(coffee){
+    return coffee;
+}
 
 let myDate = new Date();
 let today = myDate.getDay();
+let coffee  ""; 
 
-console.log(today);
+switch(today){
+    case 1:
+        today="Monday";
+        coffee = {
+            color:"pink"
+            name : "Bubble Tea",
+            pic: "bubble-tea.jpg"
+            alt:"A picture of a bubble tea",
+            day: "Monday"
+            desc: 'I Like me some bubble tea!'
+
+        };
+    break;
+    case 2:
+        today="Tuesday";
+        coffee = {
+            color:"brown"
+            name : "caramel latte",
+            pic: "caramel-latte.jpg"
+            alt:"A picture of a bubble tea",
+            day: "Tuesday"
+            desc: 'It's cold so a caramel latte sounds good right now!'
+
+        };
+    break;
+
+    
+
+
+
+    default:
+        alert("something went wrong!");
+
+
+
+}
+
+
+console.log(coffee);
+
+document.getElementById("coffee-template").innerHTML = coffeeTemplate("can you see this?");
